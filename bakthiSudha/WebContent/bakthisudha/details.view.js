273792a2896fac2d0875f517ea39277aac2b0e81
bwithbala <balakrishnan.tharumar@gmail.com>
2015-12-29 21:47:00 -0500

    3

---
+++ bakthiSudha/WebContent/bakthisudha/details.view.js
@@ -0,0 +1,27 @@
+sap.ui.jsview("bakthisudha.details", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf bakthisudha.details
+	*/ 
+	getControllerName : function() {
+		return "bakthisudha.details";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf bakthisudha.details
+	*/ 
+	createContent : function(oController) {
+		
+		
+		
+ 		return new sap.m.Page({
+			title: "Title",
+			content: [
+			
+			]
+		});
+	}
+
+});
